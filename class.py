d3a639b1eaff2992edf358b1f4236c4c4cb57c11
kiran360840 <kiranbaskota8@gmail.com>
2025-06-21 22:56:34 +0545

    Create class.py

---
+++ class.py
@@ -0,0 +1,8 @@
+class first_class:
+    first = "Kiran"
+    last = "Baskota"
+
+objct = first_class()
+print(f"Your First name: {objct.first}.")
+print(f"Your Last name: {objct.last}")
+print(f"Your full name: {objct.first} {objct.last}")
